9cc44e3cc2ac0568c83876b6bf8de2aa7fa8ef8c
AgoniLay <1394310259@qq.com>
2021-12-02 16:32:19 +0800

    Site updated: 2021-12-02 16:32:14

---
+++ scripts/injector.js
@@ -0,0 +1,16 @@
+// hexo.extend.injector.register(
+//   'body_end',
+//   `
+//   <link rel="stylesheet" href="https://cdn.staticfile.org/fancybox/3.5.7/jquery.fancybox.min.css">
+//   <script src="//cdn.jsdelivr.net/npm/minigrid@3.1.1/dist/minigrid.min.js"></script>
+//   <script src="https://cdn.staticfile.org/fancybox/3.5.7/jquery.fancybox.min.js"></script>
+// 	<script src="https://cdn.bootcdn.net/ajax/libs/lazyloadjs/3.2.2/lazyload.js"></script>
+//   <script defer src="/js/photoWall.js"></script>`
+// )
+
+// hexo.extend.injector.register('body_end', `
+//   <div id="aplayer"></div>
+//   <link defer rel="stylesheet" href="https://cdn.staticfile.org/aplayer/1.10.1/APlayer.min.css">
+//   <script src="https://cdn.staticfile.org/aplayer/1.10.1/APlayer.min.js"></script>
+//   <script defer src="/js/aplayer.js"></script>
+// `)
--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/index.html","419796bdbc4ca5c102b16054baf36bc1"]];
+var precacheConfig = [["/index.html","138ff3a9daa9130cc20303eacc0744cc"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
