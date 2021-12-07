11a1e63c272f94fb8578359617c1bd0b3c98b562
AgoniLay <1394310259@qq.com>
2021-12-07 09:50:32 +0800

    Site updated: 2021-12-07 09:50:26

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/index.html","e95d5076cfff4cd8af72b0d85d85eadc"]];
+var precacheConfig = [["/index.html","ae7b27120c16077ef422d7fa9d9499d7"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
