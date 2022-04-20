77d09c63ef0565f8e4cfe19c35c531d699295a59
张宝文 <1394310259@qq.com>
2022-04-21 00:53:04 +0800

    Site updated: 2022-04-21 00:53:02

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/index.html","513c148f78b3a3cdcb6c71fa23d2e980"]];
+var precacheConfig = [];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
